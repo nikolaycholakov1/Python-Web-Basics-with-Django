6508dfaa1382b1da891b9abdea4459751e32d9bf
Nikolay Cholakov <106109220+nikolaycholakov1@users.noreply.github.com>
2023-06-16 01:20:01 +0300

    Delete Exam prep/Exam_30_October_2022 directory

--- Exam prep/Exam_30_October_2022/Exam_30_October_2022/common/templatetags/get_profile.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from Exam_30_October_2022.car_profile.models import Profile
-
-register = template.Library()
-
-
-@register.simple_tag
-def get_user_profile():
-    return Profile.objects.first()
--- Exam prep/Exam_30_October_2022/Exam_30_October_2022/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('Exam_30_October_2022.common.urls')),
-    path('car/', include('Exam_30_October_2022.car.urls')),
-    path('profile/', include('Exam_30_October_2022.car_profile.urls')),
-]
